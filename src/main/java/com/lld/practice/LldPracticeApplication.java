9ec42fc6305179b8d803da9dd0c9d891b155b24e
aditya.raghav <raghav.placement1822@gmail.com>
2025-11-16 11:12:56 +0530

    initial commit

---
+++ src/main/java/com/lld/practice/LldPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.lld.practice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class LldPracticeApplication {
+
+	public static void main(String[] args) {
+		// Not starting spring; just using bootstrapped structure
+		System.out.println("LLD Practice Project Bootstrapped!");
+	}
+}
